82baf83f9ea5d0dae164dd5cae8ba744c83bdb7e
suruur <surrarweesh@gmail.com>
2023-10-16 09:47:21 +0000

    adding first file

---
+++ 0-printf.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - printf function
+ * @format: para 1
+ * @: format
+ * Return: number of format
+ */
+
+int _printf(const char *format, ...)
+{
+	int chars_count, i, j;
+	va_list args;
+
+	va_start(args, format);
+	chars_count = 0;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					{
+						int c = va_arg(args, int);
+
+						putchar(c);
+						chars_count++;
+						break;
+					}
+				case 's':
+					{
+						const char *str = va_arg(args, const char *);
+
+						for (j = 0; str[j] != '\0'; j++)
+						{
+							putchar(str[j]);
+							chars_count++;
+						}
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						chars_count++;
+						break;
+					}
+				default:
+					{
+						putchar('%');
+						putchar(format[i]);
+						chars_count += 2;
+					}
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			chars_count++;
+		}
+	}
+
+	va_end(args);
+
+	return (chars_count);
+}
