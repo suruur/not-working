c87135c36b6322ac39c3a7bef1e02020142f9a06
suruur <surrarweesh@gmail.com>
2023-10-20 23:24:30 +0000

    jsut p

--- main.h
+++ main.h
@@ -1,10 +1,6 @@
 #ifndef MY_HEADER
 #define MY_HEADER
 
-int _putchar(char c);
-
-int task2(const char *format, ...);
-
 int _printf(const char *format, ...);
 
 #endif
--- test/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MY_HE
-#define MY_HE
-int _printf(const char *format, ...);
-int task0(char *str);
-void task1(int no);
-int task2(int b);
-
-#endif 
